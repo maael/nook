9f712a00ab3cae6f0cf98cf2f31aa86acd510ca2
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-04-09 09:15:20 +0100

    fix: 🐛 Use process.env directly, as destructuring doesn't work

--- util/s3.ts
+++ util/s3.ts
@@ -1,15 +1,9 @@
 import { parse } from "path";
 import { S3 } from "aws-sdk";
 
-const {
-  NOOK_S3_ACCESS_KEY_ID,
-  NOOK_S3_SECRET_ACCESS_KEY,
-  NOOK_S3_BUCKET_NAME
-} = process.env;
-
 const s3 = new S3({
-  accessKeyId: NOOK_S3_ACCESS_KEY_ID,
-  secretAccessKey: NOOK_S3_SECRET_ACCESS_KEY
+  accessKeyId: process.env.NOOK_S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.NOOK_S3_SECRET_ACCESS_KEY
 });
 
 export async function upload(
@@ -18,18 +12,9 @@ export async function upload(
   file: Express.Multer.File
 ) {
   const { ext } = parse(file.originalname);
-  console.info(
-    "uploading",
-    NOOK_S3_ACCESS_KEY_ID,
-    NOOK_S3_SECRET_ACCESS_KEY,
-    NOOK_S3_BUCKET_NAME,
-    userId,
-    makerCode,
-    ext
-  );
   const result = await s3
     .upload({
-      Bucket: NOOK_S3_BUCKET_NAME!,
+      Bucket: process.env.NOOK_S3_BUCKET_NAME!,
       Key: `custom-design/${userId}/${makerCode}${ext}`,
       Body: file.buffer
     })
@@ -38,6 +23,8 @@ export async function upload(
 }
 
 export async function remove(url: string) {
-  const key = url.split(NOOK_S3_BUCKET_NAME!)[1].slice(1);
-  await s3.deleteObject({ Bucket: NOOK_S3_BUCKET_NAME!, Key: key }).promise();
+  const key = url.split(process.env.NOOK_S3_BUCKET_NAME!)[1].slice(1);
+  await s3
+    .deleteObject({ Bucket: process.env.NOOK_S3_BUCKET_NAME!, Key: key })
+    .promise();
 }
