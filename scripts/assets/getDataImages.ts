413a9ffca195af48113dfb38edb3ea937a6568ee
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-04-27 11:48:21 +0100

    feat: 🎸 Add sculpture and painting images, update other imgs

--- scripts/assets/getDataImages.ts
+++ scripts/assets/getDataImages.ts
@@ -5,6 +5,8 @@ const bugData = require("../../data/bugs.json");
 const fishData = require("../../data/fish.json");
 const recipeData = require("../../data/recipes.json");
 const fossilsData = require("../../data/fossils.json");
+const paintingsData = require("../../data/paintings.json");
+const sculpturesData = require("../../data/sculptures.json");
 
 async function getDataImages(data: any[], type: string) {
   await Promise.all(
@@ -20,11 +22,40 @@ async function getDataImages(data: any[], type: string) {
   );
 }
 
+async function getArtDataImages(data: any[], type: string) {
+  await Promise.all(
+    data
+      .filter(({ wikiImageUrls }) => wikiImageUrls)
+      .map(async ({ wikiImageUrls, name }) => {
+        try {
+          if (wikiImageUrls.forgery)
+            await pipeFile(wikiImageUrls.forgery, [
+              "images",
+              type,
+              "forgery",
+              getImageName(name)
+            ]);
+          if (wikiImageUrls.real)
+            await pipeFile(wikiImageUrls.real, [
+              "images",
+              type,
+              "real",
+              getImageName(name)
+            ]);
+        } catch (e) {
+          console.error(e.message);
+        }
+      })
+  );
+}
+
 export default async function getImages() {
   await Promise.all([
     getDataImages(bugData, "bug"),
     getDataImages(fishData, "fish"),
     getDataImages(fossilsData, "fossil"),
-    getDataImages(recipeData, "recipe")
+    getDataImages(recipeData, "recipe"),
+    getArtDataImages(paintingsData, "painting"),
+    getArtDataImages(sculpturesData, "sculpture")
   ]);
 }
